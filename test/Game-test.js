340d085c4fdfea4108078cde5cd8872b58046499
katiescruggs <katie.e.scruggs@gmail.com>
2017-10-05 19:45:54 -0600

    set up test files

---
+++ test/Game-test.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+const assert = chai.assert;
+const Game = require('../lib/Game.js');
+
+describe('Game', function() {
+
+
+
+});
---
+++ test/Snake-test.js
@@ -0,0 +1,7 @@
+const chai = require('chai');
+const assert = chai.assert;
+const Snake = require('../lib/Snake.js');
+
+describe('Snake', function() {
+
+});
