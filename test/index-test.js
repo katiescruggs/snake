deec0613805fd5ff86f86fc507fb9c03bd954b90
katiescruggs <katie.e.scruggs@gmail.com>
2017-10-07 09:35:11 -0600

    fix webpack compile file path

--- test/index-test.js
+++ test/index-test.js
@@ -1,5 +1,5 @@
-const chai = require('chai');
-const assert = chai.assert;
-const index = require('../lib/index.js');
+// const chai = require('chai');
+// const assert = chai.assert;
+// const index = require('../lib/index.js');
 
 //should this even be a file? do you need a test for index.js?
